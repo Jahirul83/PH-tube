d1d17e8fdba16f6c93ebd345ad510660841a99c6
Jahirul83 <jahirulislamcse83.com>
2023-09-02 20:16:28 +0600

    small edits

--- js/api.js
+++ js/api.js
@@ -22,13 +22,15 @@ let currentID = '0';
 
 const handleLoadCategory = async (categoryId) => {
 
+
+
     currentID = categoryId;
-    
+
     const res = await fetch(`https://openapi.programming-hero.com/api/videos/category/${categoryId}`);
     const idData = await res.json();
-    
+
     // console.log(idData.data.length);
-    console.log('sort=======>',currentID);
+    console.log('sort=======>', currentID);
 
     const cardContainer = document.getElementById('category-cards');
     cardContainer.textContent = '';
@@ -63,15 +65,15 @@ const handleLoadCategory = async (categoryId) => {
             let duration = card?.others?.posted_date;
 
             // console.log(card.others.views);
-            let videoViews =  card?.others?.views;
-           
+            let videoViews = card?.others?.views;
+
 
             if (duration > 0) {
 
-                const hourInFloat = duration/3600;
+                const hourInFloat = duration / 3600;
                 const hour = parseInt(hourInFloat);
-                const restMinutes = duration%3600;
-                const minInFloat = restMinutes/60;
+                const restMinutes = duration % 3600;
+                const minInFloat = restMinutes / 60;
                 const minutes = parseInt(minInFloat);
                 const hrs = 'hrs ';
                 const minAgo = 'min ago';
@@ -114,7 +116,7 @@ const handleLoadCategory = async (categoryId) => {
 
     }
 
-    
+
 }
 
 
@@ -122,93 +124,60 @@ const handleLoadCategory = async (categoryId) => {
 
 // sorting
 
-const sortbyView = async () =>{
+const sortbyView = async () => {
     // console.log('first');
     const res = await fetch(`https://openapi.programming-hero.com/api/videos/category/${currentID}`);
     const data = await res.json();
+
+
     const videoByViews = data.data;
 
+    videoByViews.sort((x, y) => {
+        let a = parseInt(x.others.views.replace('K', ' '));
+        a = a * 1000;
 
-    
-     videoByViews.sort((x,y)=>{
-        const a = parseInt(x.others.views.replace('K','000'));
-        const b = parseInt(y.others.views.replace('K','000'));
-        const sorted = b-a;
+        let b = parseInt(y.others.views.replace('K', ' '));
+        b = b * 1000;
+        const sorted = b - a;
         return sorted;
-        
+
     })
 
-   /*  videoByViews.sort((x, y) => {
-        const a = parseInt(x.others.views.replace('K', '000'));
-        const b = parseInt(y.others.views.replace('K', '000'));
-      
-        if (a < b) {
-          return 1;
-        } else if (a > b) {
-          return -1;
-        } else {
-          return 0;
-        }
-      }); */
-   
-      
-      
-      console.log('asdas',videoByViews)
-      
-      
-    
+
+
+    console.log('asdas', videoByViews)
+
+
+
     const cardContainer = document.getElementById('category-cards');
     cardContainer.textContent = '';
     const emptyDiv = document.getElementById('empty-div');
     emptyDiv.textContent = '';
 
-   /*  if (videoByViews.data.length === 0) {
 
 
-        const Div = document.createElement('div');
-        Div.innerHTML = `
-        <div class="my-9">
-                    <div class="flex justify-center items-center">
-                        <img src="image/Icon.png" alt="">
-                    </div>
-                    <div class="text-center">
-                    <h3 class="text-5xl">Oops!! Sorry, There is no content here</h3>
-                    </div>
-                </div>
+    videoByViews.forEach((card) => {
+        let duration = card?.others?.posted_date;
 
-        `;
-
-        emptyDiv.appendChild(Div);
-    } */
-    // else {
-        // const cardContainer = document.getElementById('category-cards');
-        // console.log(cardContainer);
-
-        // cardContainer.textContent = '';
-
-        videoByViews.forEach((card) => {
-            let duration = card?.others?.posted_date;
-
-            // console.log(card.others.views);
-            // let videoViews =  card?.others?.views;
-           
-
-            if (duration > 0) {
-
-                const hourInFloat = duration/3600;
-                const hour = parseInt(hourInFloat);
-                const restMinutes = duration%3600;
-                const minInFloat = restMinutes/60;
-                const minutes = parseInt(minInFloat);
-                const hrs = 'hrs ';
-                const minAgo = 'min ago';
-                duration = hour + hrs + minutes + minAgo;
-
-            }
+        // console.log(card.others.views);
 
 
-            const cardDiv = document.createElement('div');
-            cardDiv.innerHTML = `
+        if (duration > 0) {
+
+            const hourInFloat = duration / 3600;
+            const hour = parseInt(hourInFloat);
+            const restMinutes = duration % 3600;
+            const minInFloat = restMinutes / 60;
+            const minutes = parseInt(minInFloat);
+            const hrs = 'hrs ';
+            const minAgo = 'min ago';
+            duration = hour + hrs + minutes + minAgo;
+
+        }
+
+
+        const cardDiv = document.createElement('div');
+        cardDiv.innerHTML = `
         <div class="card h-96 bg-gray-100 shadow-xl my-2">
                     <figure class="w- mx-auto"><img class="h-40" src="${card.thumbnail}" alt="Shoes" />
     
@@ -227,19 +196,18 @@ const sortbyView = async () =>{
                         <div class="flex">
                             <p>${card?.authors[0]?.profile_name}
                                 ${card?.authors[0]?.verified ?
-                    '<i class="bg-blue-600 p-1 text-white rounded-full fa-solid fa-check"></i>' : ''}</p>
+                '<i class="bg-blue-600 p-1 text-white rounded-full fa-solid fa-check"></i>' : ''}</p>
                         </div>
                         <p>${card?.others?.views}</p>
                     </div>
                 </div>
 
         `;
-            cardContainer.appendChild(cardDiv);
+        cardContainer.appendChild(cardDiv);
 
 
-        });
+    });
 
-    // }
 
 }
 
